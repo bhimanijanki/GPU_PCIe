1428167af2b7ba78d8bfd2c41aa403a0db2f95c2
bhimanijanki <bhimani.j@husky.neu.edu>
2016-09-12 10:13:20 -0400

    Update micro_bench_UM_balB.cu

--- micro_bench_UM_balB.cu
+++ micro_bench_UM_balB.cu
@@ -107,7 +107,6 @@ seq = ms;
 printf("Time for seq transfer and execute (ms): %f\n", ms+aloc);
 printf("Time for kernel execute (ms): %f\n", msk);
 printf("Bytes for sequential transfer (bytes): %d\n", bytes);
-  printf("  max error: %e\n", maxError(a, n));
 
   // asynchronous version 1: loop over {copy, kernel, copy}
   memset(a, 0, bytes);
@@ -122,8 +121,7 @@ printf("Bytes for sequential transfer (bytes): %d\n", bytes);
   checkCuda( cudaEventRecord(stopEvent, 0) );
   checkCuda( cudaEventSynchronize(stopEvent) );
   checkCuda( cudaEventElapsedTime(&ms, startEvent, stopEvent) );
-  printf("Time for asynchronous V1 transfer and execute (ms): %f\n", ms+aloc);
-  printf("  max error: %e\n", maxError(a, n));
+  printf("Scheduling scheme type I transfer and execute (ms): %f\n", ms+aloc);
   // asynchronous version 2: 
   // loop over copy, loop over kernel, loop over copy
   memset(a, 0, bytes);
@@ -149,8 +147,7 @@ printf("Bytes for sequential transfer (bytes): %d\n", bytes);
   checkCuda( cudaEventRecord(stopEvent, 0) );
   checkCuda( cudaEventSynchronize(stopEvent) );
   checkCuda( cudaEventElapsedTime(&ms, startEvent, stopEvent) );
-  printf("Time for asynchronous V2 transfer and execute (ms): %f\n", ms+aloc);
-  printf("  max error: %e\n", maxError(a, n));
+  printf("Scheduling scheme type II transfer and execute (ms): %f\n", ms+aloc);
   printf("% Overlap (%): %f\n", (seq-ms)/seq*100);
   // cleanup
   checkCuda( cudaEventDestroy(startEvent) );
